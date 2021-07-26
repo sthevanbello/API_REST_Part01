640160d37b73417460b68e66f4e362ca554cd596
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-26 19:02:28 -0300

    Convert text to csv and xml

--- Alura.WebAPI.WebApp/Controllers/Api/LivrosController.cs
+++ Alura.WebAPI.WebApp/Controllers/Api/LivrosController.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Alura.WebAPI.WebApp.Controllers.Api
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")] // Indica que o roteamento será pelo nome do controlador
     public class LivrosController : ControllerBase
---
+++ Alura.WebAPI.WebApp/Formartters/LivroCsvFormatter.cs
@@ -0,0 +1,45 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Formartters
+{
+    public class LivroCsvFormatter : TextOutputFormatter
+    {
+        public LivroCsvFormatter()
+        {
+            var csvMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var appCsvMediaType = MediaTypeHeaderValue.Parse("application/csv");
+            SupportedMediaTypes.Add(csvMediaType);
+            SupportedMediaTypes.Add(appCsvMediaType);
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return type == typeof(LivroApi);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+
+            var livroEmCsv = "";
+            if (context.Object is LivroApi)
+            {
+                var livro = context.Object as LivroApi;
+                livroEmCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista}";
+            }
+
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroEmCsv);
+            }
+            
+        }
+    }
+}
