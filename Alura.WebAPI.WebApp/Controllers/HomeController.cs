344dfcd454f18377c3b7b795c83f565833654f32
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-29 19:32:38 -0300

    HomeController using api

--- Alura.WebAPI.WebApp/Controllers/HomeController.cs
+++ Alura.WebAPI.WebApp/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using Alura.ListaLeitura.HttpClients;
+using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.WebApp.Controllers
 {
@@ -12,27 +14,28 @@ namespace Alura.ListaLeitura.WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly IRepository<Livro> _repo;
+        private readonly LivroApiClient _livroApiClient;
 
-        public HomeController(IRepository<Livro> repository)
+        public HomeController(IRepository<Livro> repository, LivroApiClient livroApiClient)
         {
             _repo = repository;
+            _livroApiClient = livroApiClient;
         }
 
-        private IEnumerable<LivroApi> ListaDoTipo(TipoListaLeitura tipo)
+        private async Task<IEnumerable<LivroApi>> ListaDoTipo(TipoListaLeitura tipo)
         {
-            return _repo.All
-                .Where(l => l.Lista == tipo)
-                .Select(l => l.ToApi())
-                .ToList();
+            var lista =  await _livroApiClient.GetListaLeituraAsync(tipo);
+
+            return lista.Livros;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var model = new HomeViewModel
             {
-                ParaLer = ListaDoTipo(TipoListaLeitura.ParaLer),
-                Lendo = ListaDoTipo(TipoListaLeitura.Lendo),
-                Lidos = ListaDoTipo(TipoListaLeitura.Lidos)
+                ParaLer = await ListaDoTipo(TipoListaLeitura.ParaLer),
+                Lendo = await ListaDoTipo(TipoListaLeitura.Lendo),
+                Lidos = await ListaDoTipo(TipoListaLeitura.Lidos)
             };
             return View(model);
         }
