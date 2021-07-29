c4c58715753cfc95bea8db973c40c264f7086da4
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-29 19:36:26 -0300

    Created methods in the Interface ILivroApiClient

--- Alura.WebAPI.WebApp/HttpClients/ILivroApiClient.cs
+++ Alura.WebAPI.WebApp/HttpClients/ILivroApiClient.cs
@@ -1,11 +1,14 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using System.Threading.Tasks;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
 
 namespace Alura.ListaLeitura.HttpClients
 {
     public interface ILivroApiClient
     {
+        Task DeleteLivroAsync(int id);
         Task<byte[]> GetCapaLivrosync(int id);
         Task<LivroApi> GetLivroAsync(int id);
+        Task<Lista> GetListaLeituraAsync(TipoListaLeitura tipo);
     }
 }
