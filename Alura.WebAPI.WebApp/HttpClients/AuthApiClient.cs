35b676a152bd24d9152483aefdf3ad897251be22
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-31 16:22:44 -0300

    Authentication Client for WebApp

---
+++ Alura.WebAPI.WebApp/HttpClients/AuthApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Alura.ListaLeitura.Seguranca;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.HttpClients
+{
+    public class AuthApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> PostLoginAsync(LoginModel model)
+        {
+            var resposta = await _httpClient.PostAsJsonAsync("login", model);
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsStringAsync();
+        }
+    }
+}
